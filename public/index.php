879158513738797247b453871b977aef994d36fc
Dr. Daniel Glauber <dan@heightscg.com>
2026-04-01 20:39:44 -0400

    init

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<head>
+    <title>Kintek Group</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Kintek Group</h1>
+        <div class="logo-container">
+            <img src="/images/KG-logo.png" alt="Kintek Group" class="logo">
+        </div>
+    </div>
+</body>
+</html>
